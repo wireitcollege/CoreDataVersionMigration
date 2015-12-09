5e384f76304ef98fabc36deae22da3ce0f94fb57
Instructor <ios@wire.org.ua>
2015-12-09 18:44:39 +0200

    Initial Commit

---
+++ CoreDataExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataExample
+//
+//  Created by Admin on 12/9/15.
+//  Copyright © 2015 Wire IT College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
